555ceb660fb02210b3589b10a0389e3e8373bea0
Vincent Rossignol <rossignol.vince@gmail.com>
2016-06-02 12:06:26 +0200

    working plugin data

--- interface/src/sample/controller/PluginController.java
+++ interface/src/sample/controller/PluginController.java
@@ -49,6 +49,7 @@ public class PluginController {
 
     @FXML
     public void initialize(){
+        pluginList = new ArrayList<>();
         getList();
         ObservableList<Plugin> list = FXCollections.observableArrayList(pluginList);
         pluginTable.setItems(list);
