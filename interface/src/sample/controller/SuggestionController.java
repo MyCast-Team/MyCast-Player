80dba06ddff80c1f62172754869921ac28782ebd
Thomas <thomasfouan@hotmail.fr>
2016-07-08 11:12:26 +0200

    Refactor code

--- interface/src/sample/controller/SuggestionController.java
+++ interface/src/sample/controller/SuggestionController.java
@@ -16,6 +16,7 @@ import org.apache.http.util.EntityUtils;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import sample.model.Suggestion;
 import java.io.*;
 import java.util.ArrayList;
@@ -52,10 +53,10 @@ public class SuggestionController {
     private ArrayList<Suggestion> SuggestionList;
     private ArrayList<Suggestion> SuggestionMusicList;
     private String id;
+
     public SuggestionController(){
 
     }
-
 
     @FXML
     public void initialize(){
@@ -96,171 +97,110 @@ public class SuggestionController {
         authorColumn1.setCellValueFactory(cellData->cellData.getValue().DirectorProperty());*/
     }
 
-
-
     public void getid(){
         HttpClient httpclient = new DefaultHttpClient();
         HttpPost httppost = new HttpPost("http://localhost:3000/addUser");
-        HttpResponse response1 = null;
-        try {
-            response1 = httpclient.execute(httppost);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        HttpEntity entity1 = response1.getEntity();
-
-        InputStream is = null;
-        try {
-            is = entity1.getContent();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        HttpResponse response1;
+        HttpEntity entity1;
 
         String filePath = "./res/id.json";
-        FileOutputStream fos = null;
+        FileOutputStream fos;
+        InputStream is;
+        JSONParser parser = new JSONParser();
+        Object obj;
+        JSONObject jsonObject;
+
         try {
+            response1 = httpclient.execute(httppost);
+            entity1 = response1.getEntity();
+            is = entity1.getContent();
+
             fos = new FileOutputStream(new File(filePath));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
 
-        int inByte;
-        try {
+            byte[] buffer = new byte[8 * 1024];
+            int bytesRead;
             if (is != null) {
-                while((inByte = is.read()) != -1) {
-                    try {
-                        if (fos != null) {
-                            fos.write(inByte);
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+                while((bytesRead = is.read(buffer)) != -1) {
+                    fos.write(buffer, 0, bytesRead);
                 }
-            }
-
-            if (is != null) {
                 is.close();
             }
 
-            if (fos != null) {
-                fos.close();
-            }
-
-
+            fos.close();
             EntityUtils.consume(entity1);
+
+            obj = parser.parse(new FileReader("./res/id.json"));
+            jsonObject = (JSONObject) obj;
+
+            id = jsonObject.get("id").toString();
+            System.out.println(id);
         } catch (IOException e) {
             e.printStackTrace();
-        }
-        JSONParser parser = new JSONParser();
-
-        try {
-
-            Object obj = parser.parse(new FileReader("./res/id.json"));
-
-            JSONObject jsonObject = (JSONObject) obj;
-
-
-            id=jsonObject.get("id").toString();
-            System.out.println(id);
-
-
-
-        } catch (Exception e) {
+        } catch (ParseException e) {
             e.printStackTrace();
         }
-
     }
+
     public void getList(String path,String http){
         HttpClient httpclient = new DefaultHttpClient();
         HttpGet httpGet = new HttpGet("http://localhost:3000/1/"+http);
-        HttpResponse response1 = null;
-        try {
-            response1 = httpclient.execute(httpGet);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        HttpEntity entity1 = response1.getEntity();
-
-        InputStream is = null;
-        try {
-            is = entity1.getContent();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        HttpResponse response1;
+        HttpEntity entity1;
 
         String filePath = "./res/"+path;
-        FileOutputStream fos = null;
+        FileOutputStream fos;
+        InputStream is;
+
         try {
+            response1 = httpclient.execute(httpGet);
+            entity1 = response1.getEntity();
+            is = entity1.getContent();
+
             fos = new FileOutputStream(new File(filePath));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
 
-        int inByte;
-        try {
+            byte[] buffer = new byte[8 * 1024];
+            int bytesRead;
             if (is != null) {
-                while((inByte = is.read()) != -1) {
-                    try {
-                        if (fos != null) {
-                            fos.write(inByte);
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+                while((bytesRead = is.read(buffer)) != -1) {
+                    fos.write(buffer, 0, bytesRead);
                 }
-            }
-
-            if (is != null) {
                 is.close();
             }
 
-            if (fos != null) {
-                fos.close();
-            }
-
-
+            fos.close();
             EntityUtils.consume(entity1);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        try {
             readPlugin(path);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void readPlugin(String path) throws IOException {
+    public void readPlugin(String path) {
+
         JSONParser parser = new JSONParser();
+        Object obj;
+        JSONArray jsonArray;
 
         try {
+            obj = parser.parse(new FileReader("./res/"+path));
+            jsonArray = (JSONArray) obj;
 
-            Object obj = parser.parse(new FileReader("./res/"+path));
-
-            JSONArray jsonArray = (JSONArray) obj;
-
+            JSONObject jsonObject;
             for (Object JsonItem : jsonArray) {
-                JSONObject jsonObject = (JSONObject) JsonItem;
+                jsonObject = (JSONObject) JsonItem;
                 //System.out.println(jsonObject.get("film").toString());
-                if (path == "filmuser.json") {
+                if (path.equals("filmuser.json")) {
                     SuggestionList.add(new Suggestion(jsonObject.get("film").toString(), jsonObject.get("director").toString(), jsonObject.get("length").toString(), jsonObject.get("date").toString(),jsonObject.get("type").toString(),"NULL"));
-                }else{
-
+                } else {
                     System.out.println(jsonObject.get("type").toString());
                     SuggestionMusicList.add(new Suggestion(jsonObject.get("title").toString(), jsonObject.get("singer").toString(), jsonObject.get("length").toString(),jsonObject.get("date").toString(),jsonObject.get("type").toString(),jsonObject.get("producer").toString()));
                 }
-
             }
-
-
-        } catch (Exception e) {
+        } catch (ParseException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
