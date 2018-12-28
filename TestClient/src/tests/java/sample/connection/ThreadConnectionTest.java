eb0390a6a8bb8d3c01c8601ad0f5504d80442a22
thomasfouan <thomasfouan@hotmail.fr>
2018-12-28 17:07:29 +0100

    add test for ThreadConnection class

---
+++ TestClient/src/tests/java/sample/connection/ThreadConnectionTest.java
@@ -0,0 +1,30 @@
+package sample.connection;
+
+import org.junit.Test;
+
+import static org.mockito.Mockito.*;
+import static sample.connection.socket.ServerSocketService.StopReason.STOP_DUE_TO_NOMINAL_EXIT;
+
+public class ThreadConnectionTest {
+
+    @Test
+    public void should_not_stop_server_when_stop_dead_thread() throws InterruptedException {
+        ConnectionHandler connectionHandlerMock = mock(ConnectionHandler.class);
+        ThreadConnection threadConnection = new ThreadConnection(connectionHandlerMock);
+
+        threadConnection.stop();
+
+        verify(connectionHandlerMock, never()).stopServer(STOP_DUE_TO_NOMINAL_EXIT);
+    }
+
+    @Test
+    public void should_stop_server_when_stop_alive_thread() throws InterruptedException {
+        ConnectionHandler connectionHandlerMock = mock(ConnectionHandler.class);
+        ThreadConnection threadConnection = new ThreadConnection(connectionHandlerMock);
+
+        threadConnection.start();
+        threadConnection.stop();
+
+        verify(connectionHandlerMock).stopServer(STOP_DUE_TO_NOMINAL_EXIT);
+    }
+}
